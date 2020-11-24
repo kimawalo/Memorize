99a1cb4cf388b6b603cdfcd7c80cf37ac7c2d4a3
kimawalo <kimawalo@gmail.com>
2020-11-24 08:11:38 +0200

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Jamshid Azizov on 24.11.2020.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
